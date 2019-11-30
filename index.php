bbac514cb2453487d02238090d33734b47aaa066
oldemar <oldemar_junior@hotmail.com>
2019-11-29 21:35:33 -0500

    api calling to login fixes

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $settings = null;
 $envVars = null;
-$results = null;
+$results = 'Function argument not passed...';
 
 include('./settings/Settings.php');
 include('./settings/Database.php');
@@ -10,26 +10,17 @@ include ('./getVars.php');
 
 $db = new DB($database[$settings['env']]);
 
-if ( $envVars['function'] = 'login' ) {
+if ( $envVars['function'] == 'login' ) {
 
     if (!isset($envVars['username']) || !isset($envVars['password']) ) {
         $results = 'Not enough arguments..';
-    }
-    else {
+    } else {
         $u = $envVars['username'];
         $p = $envVars['password'];
         $db->Query("SELECT 
 	* 
 FROM 
 	users as u
-LEFT JOIN
-    profiles as p
-ON 
-	u.profile_id = p.id
-LEFT JOIN
-    roles as r
-ON
-	u.role_id = r.id
 WHERE 
 	TRIM(login) = '$u' AND
     TRIM(password) = '$p'");
